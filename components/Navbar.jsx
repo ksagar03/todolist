70d68909c558a942617f9e1d7aeb550d83aab15a
ksagar03 <sagar22krishna03@gmail.com>
2023-12-02 23:46:00 +0530

    Started working on new project Todo list to learn Nextjs

---
+++ components/Navbar.jsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+const Navbar = () => {
+  return (
+    <div>
+      <header className="text-gray-400 bg-gray-900 body-font">
+        <div className="container mx-auto flex flex-wrap p-3 flex-col md:flex-row items-center">
+          <a className="flex title-font font-medium items-center text-white mb-2 md:mb-0">
+            <Image src={"/images/To Do List.gif"} width="25" height="25" />
+            <span className="ml-3 text-xl">Todo</span>
+          </a>
+          <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+            <Link href={"/"} className="mr-5 hover:text-white">
+              Log In
+            </Link>
+            <Link href={"/AddTodo"} className="mr-5 hover:text-white">
+              Add New Todo 
+            </Link>
+          </nav>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+import Navbar from "@/components/Navbar";
+import "@/styles/globals.css";
+import Head from "next/head";
+import Link from "next/link";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+      <link rel="icon" href="/images/todo_favi.png" sizes="any" />
+        <title>Todo list</title>
+      </Head>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
